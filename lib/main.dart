a2e4a7c905cb83451ec0bf858b20f64f95478a51
yusufbingol <yusufbingol1994@gmail.com>
2021-09-27 20:08:03 +0300

    Tododo App >> Todo App

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tododo App',
+      title: 'Todo App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.ubuntuTextTheme(Theme.of(context).textTheme),
